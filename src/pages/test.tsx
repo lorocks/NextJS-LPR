23bad09cd64ffa558292dec2c433696e739b931a
lorocks <63993526+lorocks@users.noreply.github.com>
2023-05-27 09:43:39 +0400

    Update test.tsx

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -26,7 +26,7 @@ function Test({ info }) {
 
       let imageBase64Stringsep = base64String;
 
-      // alert(imageBase64Stringsep);
+      alert(imageBase64Stringsep);
       fetch("https://ll753-flaskmlbackendlpr.hf.space/uploadlicense", {
         method: "POST",
         headers: {
